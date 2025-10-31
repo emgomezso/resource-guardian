aea0dba0c23ab198eba93633b7852b1a73b24910
Eduardo Mauricio Gomez Solis <emgomezso@outlook.com>
2025-10-31 09:39:30 -0600

    Fixes

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -162,7 +162,7 @@ private function initializeDatabase()
         
         $db = new SQLite3($this->dbPath);
         
-        $schemaPath = pm_Context::getPath() . '/resources/database.sql'; 
+        $schemaPath = pm_Context::getBaseUrl() . '/resources/database.sql'; 
         
         if (file_exists($schemaPath)) {
             $schema = file_get_contents($schemaPath);
