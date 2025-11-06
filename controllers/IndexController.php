58f2da2b3ee784cbc49bb6fcd580662991ed3069
Eduardo Mauricio Gomez Solis <emgomezso@outlook.com>
2025-11-06 10:15:06 -0600

    Fixes

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -13,7 +13,7 @@ class IndexController extends pm_Controller_Action
         parent::init();
         
         // Set database path
-        $this->dbPath = pm_Context::getVarDir() . '/db/metrics.db';
+        $this->dbPath = pm_Context::getVarDir() . '/var/db/metrics.db';
         
         // Initialize database if not exists
         if (!file_exists($this->dbPath)) {
